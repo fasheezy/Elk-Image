324dc3aafd9aed95aaf7da147e6a09f3fc7ccfbc
JT Coder <53548183+fasheezy@users.noreply.github.com>
2023-06-15 11:08:46 -0700

    Delete Config directory

--- Config/virtual_env/tet/NLTKtest.py
+++
@@ -1,5 +0,0 @@
-import nltk 
-from nltk.tokenize import word_tokenize 
-nltk.download('punkt')
-text = "What is the meaning of this I am trying to escape and you bring me back"
-print(word_tokenize(text))
